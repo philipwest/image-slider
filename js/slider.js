63eead0de1e2cb9db61807e0abcdedfde8d491b2
philipwest <philip@westendorp.org>
2014-06-04 07:52:07 -0700

    Fixing loop for prev and next functions

--- js/slider.js
+++ js/slider.js
@@ -1,59 +1,70 @@
 var initSlide = 1;
-var nextSlide = initSlide + 1;
+var nextSlide;
 var count;
 var slides;
 var loop;
+var fadeSpeed;
 
 $(document).ready(function(){
-	$('.slide:nth-of-type('+initSlide+')').fadeIn(300);
+	$('.slide:nth-of-type('+initSlide+')').show();
 	startSlider();
 
 });
 
 function startSlider(){
+//console.log("startSlider");
 	count = $('.slider > .slide').size();
 	slides = $('.slide');
 
 	loop = setInterval(function(){
-		if(nextSlide > count){
-			nextSlide = 1;
-		}else if(nextSlide < 0){
-			nextSlide = count;
-		}
-		
+		setNext(initSlide + 1);
 		showSlide(nextSlide);
-
-	}, 1200);
+		
+	}, 1500);
 
 }
 
+function setNext(val){
+	if(val > count){
+		nextSlide = 1;
+	}else if(val <= 0){
+		nextSlide = count;
+	}else{
+		nextSlide = val;
+	}
+//console.log("setNext: " + nextSlide);
+}
+
 function showSlide(slide){
+//console.log("showSlide: " + slide);
 
 	slides.eq(initSlide - 1).toggle();
 	slides.eq(slide - 1).fadeIn(500);
 
 	initSlide = slide;
-	nextSlide = slide + 1;
-
+	setNext(slide + 1);
 }
 
 function stopSlider(){
+//console.log("stopSlider");
 	window.clearInterval(loop);
 
 }
 
 function prev(){
+//console.log("prev");
 	stopSlider();
-	nextSlide = initSlide - 1;
-//	console.log(nextSlide);
+	setNext(initSlide - 1);
+//console.log(nextSlide);
 	showSlide(nextSlide);
 	startSlider();
 }
 
 function next(){
+//console.log("next");
 	stopSlider();
-	nextSlide = initSlide + 1;
-//	console.log(nextSlide);
+	setNext(initSlide + 1);
+//console.log(nextSlide);
 	showSlide(nextSlide);
 	startSlider();
 
